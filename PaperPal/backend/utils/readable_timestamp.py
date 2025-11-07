4a5949fbf70c9050e0fd2bb70f36c65aca662c66
Sanjay Singh <san.singhsanjay@gmail.com>
2025-11-07 14:22:58 +0530

    Updated

--- PaperPal/backend/utils/readable_timestamp.py
+++
@@ -1,7 +0,0 @@
-# python imports
-from datetime import datetime, timezone
-
-# get readable timestamp of "now"
-def get_readable_timestamp():
-    timestamp = datetime.now(timezone.utc).strftime('%d-%m-%Y %H:%M:%S UTC')
-    return timestamp
--- PaperPal/backend/utils/root_resp.py
+++
@@ -1,6 +0,0 @@
-# python imports
-from pydantic import BaseModel
-
-# root endpoint - Response model
-class RootResponseModel(BaseModel):
-    response: str
