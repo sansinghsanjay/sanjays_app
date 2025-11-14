d3e4095b93979564d0209389d57f5ce8a9bf4b37
Sanjay Singh <san.singhsanjay@gmail.com>
2025-11-14 22:58:21 +0530

    Updated

--- frontend/utils/timestamp.py
+++
@@ -1,7 +0,0 @@
-# python imports
-from datetime import datetime, timezone
-
-# return current timestamp in readable form
-def get_current_timestamp():
-    timestamp = datetime.now(timezone.utc).strftime("%d-%m-%Y %H:%M:%S UTC")
-    return timestamp
